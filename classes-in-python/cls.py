badc99decf919b3ef019b70541c6fad7266a1dc5
Nsikak Imoh <drmacsika@gmail.com>
2022-09-17 23:46:12 +0100

    Class in OOP in Python

---
+++ classes-in-python/cls.py
@@ -0,0 +1,38 @@
+# Example 1
+
+class Car:
+	# class variable
+    speed_measurement = "KM/hr"
+    
+    def __init__(self, model, brand, color):
+        # data members (instance variables)
+        self.model = model
+        self.brand = brand
+        self.color = color
+
+    # Behavior (instance methods)
+    def info(self):
+        print('Model:', self.model, 'Brand:', self.brand, 'Color:', self.color)
+
+
+# Example 2
+class Car:
+	# class variable
+    speed_measurement = "KM/hr"
+
+    def __init__(self, model, brand, color):
+        # data members (instance variables)
+        self.model = model
+        self.brand = brand
+        self.color = color
+
+    # Behavior (instance methods)
+    def info(self):
+        print('Model:', self.model, 'Brand:', self.brand, 'Color:', self.color)
+
+	# class method
+    @classmethod
+    def set_speed_measurement(cls, new_measurement):
+        # modify class variable
+        cls.school_name = new_measurement
+        return None
